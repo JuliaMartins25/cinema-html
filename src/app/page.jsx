c867b9dd8d381724655d89e1d5d29190195ab97b
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-03-11 11:02:09 -0300

    feat: create dashboard content component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import Header from '@/components/header';
 import styles from './page.module.css';
+import Sidebar from '@/components/sidebar';
+import DashboardContent from '@/components/dashboardContent';
 
 export default function Home() {
   return (
 <div>
 <Header />
+<main className={styles.dashboardContainer}>
+<Sidebar />
+
+<DashboardContent />
+</main>
+
+
 </div>
   );
 
